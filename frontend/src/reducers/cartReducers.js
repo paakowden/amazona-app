14f834cb4759e980880340090cd5e9fd9c03ab58
paakowden <paakowden1114@gmail.com>
2021-12-11 21:30:20 +0000

    Implemented place order action

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -4,6 +4,7 @@ import {
   CART_SAVE_PAYMENT_METHOD,
   CART_SAVE_SHIPPING_ADDRESS,
 } from "../constants/cartConstants";
+import { CART_EMPTY } from "../constants/orderConstants";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
@@ -34,6 +35,11 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
       return {
         ...state,
         paymentMethod: action.payload,
+      };
+    case CART_EMPTY:
+      return {
+        ...state,
+        cartItems: [],
       };
     default:
       return state;
