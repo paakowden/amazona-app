e50c156c1be2baa294b071eaa80b106edc3ce3a6
paakowden <paakowden1114@gmail.com>
2022-01-13 17:50:44 +0000

    Added seller page to app

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -11,7 +11,10 @@ productRouter.get(
   expressAsyncHandler(async (req, res) => {
     const seller = req.query.selle || "";
     const sellerFilter = seller ? { seller } : {};
-    const products = await Product.find({ ...sellerFilter });
+    const products = await Product.find({ ...sellerFilter }).populate(
+      "seller",
+      "seller.name seller.logo"
+    );
     res.send(products);
   })
 );
@@ -19,7 +22,7 @@ productRouter.get(
 productRouter.get(
   "/seed",
   expressAsyncHandler(async (req, res) => {
-    await Product.remove({});
+    //await Product.remove({});
     const createdProducts = await Product.insertMany(data.products);
     res.send({ createdProducts });
   })
@@ -28,8 +31,10 @@ productRouter.get(
 productRouter.get(
   "/:id",
   expressAsyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
+    const product = await Product.findById(req.params.id).populate(
+      "seller",
+      "seller.name seller.logo seller.rating seller.numReviews"
+    );
     if (product) {
       res.send(product);
     } else {
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -7,14 +7,26 @@ function Product(props) {
   return (
     <div key={product._id} className="card">
       <Link to={`/product/${product._id}`}>
-        <img className="medium" src={product.image} alt={product.name} />
+        <img
+          className="medium"
+          src={product.image}
+          alt={product.name}
+          style={{ width: "850px", height: "400px" }}
+        />
       </Link>
       <div className="card-body">
         <Link to={`/product/${product._id}`}>
           <h2>{product.name}</h2>
         </Link>
         <Rating rating={product.rating} numReviews={product.numReviews} />
-        <div className="price">${product.price}</div>
+        <div className="row">
+          <div className="price">${product.price}</div>
+          <div>
+            <Link to={`seller/${product.seller._id}`}>
+              {product.seller.seller.name}
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
